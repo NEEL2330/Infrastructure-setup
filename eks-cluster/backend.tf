ec0cd769712be2e303f325d1db28ec3628017410
Neel <neelbera1107@gmail.com>
2026-05-16 13:06:54 +0530

    Github action added

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "neel-remote-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
 }
